644838a0632ad4bccc9fa83a6230dc6f377968dd
bekhairga <irgashevbehzod@gmail.com>
2020-01-30 09:56:26 +0500

    updated one section and added new section travel

--- app/js/main.js
+++ app/js/main.js
@@ -4,9 +4,9 @@ $(function () {
         fade: true,
         prevArrow: '<img class="slider-arrows slider-arrows-left"  src="img/backward.svg" alt="arrow-right">',
         nextArrow: '<img class="slider-arrows slider-arrows-right" src="img/upward.svg" alt="arrow-right">',
-        asNavFor: '.slider-dots'
+        asNavFor: '.slider-dotshead'
     });
-    $('.slider-dots').slick({
+    $('.slider-dotshead').slick({
         slidesToShow: 4,
         slidesToScroll: 4,
         asNavFor: '.header-slider'
@@ -16,6 +16,22 @@ $(function () {
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 1,
+        prevArrow: '<img class="slider-arrows slider-arrows-left"  src="img/backward.svg" alt="arrow-right">',
+        nextArrow: '<img class="slider-arrows slider-arrows-right" src="img/upward.svg" alt="arrow-right">',
+        asNavFor: '.slider-map',
+
+    })
+    $('.slider-map').slick({
+        infinite: true,
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.surf-slider',
+        focusOnSelect: true,
+    })
+    $('.travel__slider').slick({
+        infinite: true,
+        fade: true,
         prevArrow: '<img class="slider-arrows slider-arrows-left"  src="img/backward.svg" alt="arrow-right">',
         nextArrow: '<img class="slider-arrows slider-arrows-right" src="img/upward.svg" alt="arrow-right">',
     })
